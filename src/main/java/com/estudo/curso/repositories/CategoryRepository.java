aaa4f0639ccb805b42d02e849d371460de3dbd5f
Osmarhair <89876986+Osmarhair@users.noreply.github.com>
2024-06-27 19:01:23 -0300

    category pt1

---
+++ src/main/java/com/estudo/curso/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.estudo.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estudo.curso.entidades.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
